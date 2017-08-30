dffa46ed53093b658c58fe7460fc194690bac6b1
mmf_kupl <ikuplevich@mail.ru>
2017-08-30 15:58:13 +0300

    upd

---
+++ APP.js
@@ -0,0 +1,21 @@
+var APP = {
+    instruments: {
+        currencies: 'currencies',
+        timeFrame: 'timeFrame',
+        typeOfGraph: 'typeOfGraph',
+        visualSettings: 'visualSettings',
+        graphicalElements: 'graphicalElements',
+        zoom: 'zoom'
+    },
+
+    currencies: {
+        OIL: 'oil',
+        GOLD: 'gold',
+        USD: 'US dollar',
+        EUR: 'euro',
+        GBR: 'pound sterling',
+        CAD: 'canadian dollar',
+        AUD: 'australian dollar',
+        BTC: 'bitcoin'
+    }
+};
---
+++ graph.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var Graph = {
+    getDimensions: function () {
+        
+    }    
+};
